c50972fa7cd0af643c385916a6eece834ecce87e
doombubbles <obsidiantoolzg@gmail.com>
2024-02-10 22:09:27 -0800

    Add example door opening functionality

---
+++ DungeonCat/Assets/Scripts/Components/DoorEntity.cs
@@ -0,0 +1,36 @@
+﻿using Scripts.Data;
+using UnityEngine;
+namespace Scripts.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DoorEntity : EntityComponent<DoorData>
+    {
+        public SpriteRenderer spriteRenderer;
+        
+        public Sprite openSprite;
+
+        public Sprite closedSprite;
+
+        public bool IsOpen => data.isOpen;
+
+        public void SetOpen(bool isOpen)
+        {
+            data.isOpen = isOpen;
+            spriteRenderer.sprite = isOpen ? openSprite : closedSprite;
+        }
+
+        public override void SyncFromData()
+        {
+            base.SyncFromData();
+            SetOpen(data.isOpen);
+        }
+
+        protected override void OnValidateInEditor()
+        {
+            SetOpen(data.isOpen);
+            base.OnValidateInEditor();
+        }
+    }
+}
