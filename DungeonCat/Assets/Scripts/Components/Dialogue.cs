0149e09707642cb11855afc6c1f6537bc038a0ac
markpock <markpock@uw.edu>
2024-02-13 19:34:29 -0800

    Finalized the backend implementation of a generalized dialogue branching system

--- DungeonCat/Assets/Scripts/Components/Dialogue.cs
+++ DungeonCat/Assets/Scripts/Components/Dialogue.cs
@@ -12,24 +12,17 @@ namespace Scripts.Components
     {
         private Interaction? _currentInteraction;
         private IEnumerator? _coroutine;
-        public const string DefaultDialogueOption = "MEOW";
+        private bool _inInteractionChain;
+        public const string DefaultDialogueOption = "meow";
         public void Awake()
         {
             gameObject.SetActive(true);
-            data.text_vis.text = "blank";
-            data.author_vis.text = "blank";
+            data.text_vis.text = "";
+            data.author_vis.text = "";
         }
 
-        public void StartInteraction(Interaction? interaction)
+        public void StartInteraction(Interaction interaction)
         {
-            if (interaction == null)
-            {
-                gameObject.SetActive(false);
-                return;
-            }
-            
-            gameObject.SetActive(true);
-
             if (_currentInteraction == null)
             {
                 _currentInteraction = interaction;
@@ -38,14 +31,25 @@ namespace Scripts.Components
             {
                 data.Interactions.Enqueue(interaction);
             }
+            if (_inInteractionChain) return;
+            _inInteractionChain = true;
+            CurrentInteraction();
+        }
 
-            var (author, sentence) = _currentInteraction.First().GetValueOrDefault(("", ""));
+        private void CurrentInteraction()
+        {
+            if (!_inInteractionChain) return;
+            gameObject.SetActive(_currentInteraction == null);
+            if (_currentInteraction != null)
+            {
+                Display(_currentInteraction.First());
+                return;
+            }
             if (_coroutine != null)
             {
                 StopCoroutine(_coroutine);
             }
-            _coroutine = TypeSentence(author, sentence);
-            StartCoroutine(_coroutine);
+            _inInteractionChain = false;
         }
 
         private void ProgressInteraction(string dialogueOption)
@@ -54,29 +58,131 @@ namespace Scripts.Components
             var result = _currentInteraction.GetNext(dialogueOption);
             if (result == null)
             {
-                _currentInteraction = null;
-                var nextInteraction = data.Interactions.Dequeue();
-                StartInteraction(nextInteraction);
+                _currentInteraction = data.Interactions.Count > 0 ? data.Interactions.Dequeue() : null;
+                CurrentInteraction();
+                return;
             }
-            else
+            Display(result);
+        }
+
+        private void Display(DialogueLine? line)
+        {
+            if (_coroutine != null)
             {
-                (data.text_vis.text, data.author_vis.text) = result.Value;
+                StopCoroutine(_coroutine);
             }
+            if (line == null) return;
+            _coroutine = TypeSentence(line); 
+            StartCoroutine(_coroutine); 
         }
         
-        private IEnumerator TypeSentence(string author, string sentence)
+        private IEnumerator TypeSentence(DialogueLine line)
         {
-            data.author_vis.text = author;
+            data.author_vis.text = line.author;
+            data.author_vis.color = line.authorColor;
             data.text_vis.text = "";
-            foreach (char letter in sentence)
+            data.text_vis.color = line.textColor;
+            foreach (var letter in line.text)
             {
                 data.text_vis.text += letter;
                 yield return new WaitForSeconds(0.05f);
             }
             yield return new WaitUntil(() => InputManager.Actions.Player.Interact.IsPressed());
             data.text_vis.text = "";
+            data.text_vis.color = Color.white;
             data.author_vis.text = "";
+            data.text_vis.color = Color.white;
             ProgressInteraction(DefaultDialogueOption);
+        }
+    }
+    
+    [Serializable]
+    public class DialogueLine
+    {
+        public string author;
+        public string text;
+
+        public Color authorColor = Color.white;
+        public Color textColor = Color.white;
+        // These are the dialogue options of the cat (or response options, anyway).
+        // Most of the time, the default dialogue option to progress to the next
+        // next will be 'meow'.
+        // Provisional upper limit of four dialogue options.
+        private Dictionary<string, (Color, DialogueLine)> _options = new();
+        // A node in an interaction takes these actions when it is reached.
+        // This is to signal choices that might affect the character with whom the
+        // dialogue is being taken.
+        private List<Action> _callbacksOnStart = new(), _callbacksOnEnd = new();
+
+        public DialogueLine(string author, string text)
+        {
+            this.author = author;
+            this.text = text;
+        }
+
+        public DialogueLine AuthorColor(Color color)
+        {
+            authorColor = color;
+            return this;
+        }
+        
+        public DialogueLine TextColor(Color color)
+        {
+            textColor = color;
+            return this;
+        }
+
+        public void AddNext(DialogueLine node)
+        {
+            _options.Add(Dialogue.DefaultDialogueOption, (Color.white, node));
+        }
+        
+        public void AddNext(string option, DialogueLine node)
+        {
+            _options.Add(option, (Color.white, node));
+        }
+        
+        public void AddNext(string option, Color color, DialogueLine node)
+        {
+            _options.Add(option, (color, node));
+        }
+
+        public DialogueLine? GetNext(string option)
+        {
+            EndCallbacks();
+            DialogueLine? next = _options.ContainsKey(option) ? _options[option].Item2 : null;
+            if (next == null) return next;
+            next.StartCallbacks();
+            return next;
+        }
+
+        // A node's start callbacks are called by the node that
+        // creates it.
+        public void StartCallbacks()
+        {
+            foreach (Action callback in _callbacksOnStart)
+            {
+                callback.Invoke();
+            }
+        }
+        
+        // A node is responsible for calling its own EndCallbacks.
+        private void EndCallbacks()
+        {
+            foreach (Action callback in _callbacksOnEnd)
+            {
+                callback.Invoke();
+            }
+        }
+
+        public void AddStartCallBack(Action action)
+        {
+            _callbacksOnStart.Add(action);
+        }
+        
+        public void AddEndCallBack(Action action)
+        {
+            _callbacksOnEnd.Add(action);
         }
     }
 
@@ -88,30 +194,29 @@ namespace Scripts.Components
     [Serializable]
     public class Interaction
     {
-        private Node? _start;
-        private Node? _curr;
+        private DialogueLine? _start;
+        private DialogueLine? _curr;
         // A node doesn't know whether it's the end, so to end an interaction, we have a
         // separate set of callbacks.
         private List<Action> _onEndCallbacks = new();
         
-        public (string, string)? First()
+        public DialogueLine? First()
         {
             if (_curr == null) return null;
             _curr.StartCallbacks();
-            return (_curr.author, _curr.text);
+            return _curr;
         }
         
-        public (string, string)? GetNext(string dialogueOption)
+        public DialogueLine? GetNext(string dialogueOption = Dialogue.DefaultDialogueOption)
         {
             if (_curr == null) return null;
-            _curr.EndCallbacks();
             _curr = _curr.GetNext(dialogueOption);
             if (_curr == null)
             {
                 EndCallbacks();
                 return null;
             }
-            return (_curr.author, _curr.text);
+            return _curr;
         }
 
         public void AddEndCallback(Action action)
@@ -131,91 +236,23 @@ namespace Scripts.Components
         public static Interaction Basic(string author, IEnumerable<string> iterator)
         {
             Interaction interaction = new();
-            Node curr = new(author, "");
+            DialogueLine curr = new(author, "");
+            DialogueLine? next = null;
             interaction._curr = curr;
             
             foreach (var text in iterator)
             {
                 if (text == null) return interaction;
+                if (next != null)
+                {
+                    curr.AddNext(next);
+                    curr = next;
+                }
                 curr.text = text;
-                Node next = new(author, "");
-                curr.AddNext(next);
-                curr = next;
+                next = new(author, "");
             }
 
             return interaction;
-        }
-        
-        [Serializable]
-        private class Node
-        {
-            // The name of the author (may be separate from their name as a character, for
-            // instance if a character's name is hidden from the player).
-            public string author;
-            // The current string being displayed as the dialogue text.
-            public string text;
-            // These are the dialogue options of the cat (or response options, anyway).
-            // Most of the time, the default dialogue option to progress to the next
-            // next will be 'meow'.
-            private Dictionary<string, Node> _options = new();
-            // A node in an interaction takes these actions when it is reached.
-            // This is to signal choices that might affect the character with whom the
-            // dialogue is being taken.
-            private List<Action> _callbacksOnStart = new(), _callbacksOnEnd = new();
-
-            public Node(string author, string text)
-            {
-                this.author = author;
-                this.text = text;
-            }
-
-            public void AddNext(Node node)
-            {
-                _options.Add(Dialogue.DefaultDialogueOption, node);
-            }
-            
-            public void AddNext(string option, Node node)
-            {
-                _options.Add(option, node);
-            }
-
-            public Node? GetNext(string option)
-            {
-                EndCallbacks();
-                Node? next = _options[option];
-                if (next == null) return next;
-                next.StartCallbacks();
-                return next;
-            }
-
-            // A node's start callbacks are called by the entity that
-            // creates it.
-            public void StartCallbacks()
-            {
-                foreach (var callback in _callbacksOnStart)
-                {
-                    callback.Invoke();
-                }
-            }
-            
-            // A node is responsible for calling its own EndCallbacks.
-            public void EndCallbacks()
-            {
-                foreach (var callback in _callbacksOnEnd)
-                {
-                    callback.Invoke();
-                }
-            }
-
-            public void AddStartCallBack(Action action)
-            {
-                _callbacksOnStart.Add(action);
-            }
-            
-            public void AddEndCallBack(Action action)
-            {
-                _callbacksOnEnd.Add(action);
-            }
         }
     }
 }
