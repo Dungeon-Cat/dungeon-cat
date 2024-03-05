cd29da148a38f1e95f85555749f1b967b0e82128
Taryn Neal <tarynneal10@gmail.com>
2024-03-05 14:17:39 -0800

    Add animation triggers and fix colliders

--- DungeonCat/Assets/Scripts/Components/AnimationCat.cs
+++ DungeonCat/Assets/Scripts/Components/AnimationCat.cs
@@ -4,36 +4,27 @@ namespace Scripts.Components
 {
     public class AnimationCat : MonoBehaviour
     {
-        Animator cat_Animator;
+        Animator catAnimator;
 
         void Start()
         {
             //Get the Animator attached to the GameObject you are intending to animate.
-            cat_Animator = gameObject.GetComponent<Animator>();
+            catAnimator = gameObject.GetComponent<Animator>();
         }
 
         void Update()
         {
             //Press WASD to trigger walk
-            if (UnityEngine.Input.GetKey(KeyCode.W) || UnityEngine.Input.GetKey(KeyCode.A) ||
-                UnityEngine.Input.GetKey(KeyCode.S) || UnityEngine.Input.GetKey(KeyCode.D))
+            if (UnityEngine.Input.GetKey(KeyCode.W))
             {
-                // Reset Idle trigger
-                cat_Animator.ResetTrigger("Idle");
-
-                //Send the message to the Animator to activate the trigger parameter named "Walk"
-                cat_Animator.SetTrigger("Walk");
+                catAnimator.SetBool("Walking", true);
             }
 
-            // if (!(UnityEngine.Input.GetKey(KeyCode.W) || UnityEngine.Input.GetKey(KeyCode.A) ||
-            //     UnityEngine.Input.GetKey(KeyCode.S) || UnityEngine.Input.GetKey(KeyCode.D)))
-            // {
-            //     //Reset the "Wall" trigger
-            //     cat_Animator.ResetTrigger("Walk");
-            //
-            //     //Send the message to the Animator to activate the trigger parameter named "Idle"
-            //     cat_Animator.SetTrigger("Idle");
-            // }
+            if ( UnityEngine.Input.GetKey(KeyCode.A) ||
+                UnityEngine.Input.GetKey(KeyCode.S) || UnityEngine.Input.GetKey(KeyCode.D))
+            {
+                catAnimator.SetBool("Walking", false);
+            }
         }
     }
 }
