af0368126a478ff6059bf516de9dec1d23521462
Brandon <35272064+jesssup@users.noreply.github.com>
2024-03-05 19:46:29 -0800

    Room 3 Wall Functionality

---
+++ DungeonCat/Assets/Scripts/Components/Room3/Room3DungeonLevel.cs
@@ -0,0 +1,35 @@
+﻿using Scripts.Components.CommonEntities;
+using Scripts.Data;
+using UnityEngine;
+
+namespace Scripts.Components.Room3
+{
+    public class Room3DungeonLevel : DungeonLevel
+    {
+        [HideInInspector]
+        public bool switchState;
+        
+        public Switch[] switches;
+        public ToggleWall[] blueWalls;
+        public ToggleWall[] redWalls;
+
+        public void OnSwitchChange()
+        {
+            switchState = !switchState; 
+            foreach (var _switch in switches)
+            {
+                _switch.SetOpen(switchState);
+            }
+
+            foreach (var wall in blueWalls)
+            {
+                wall.SetOpen(switchState);
+            }
+
+            foreach (var wall in redWalls)
+            {
+                wall.SetOpen(!switchState);
+            }
+        }
+    }
+}
