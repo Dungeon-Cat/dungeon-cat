6708c418eb731e497f2daec455fa1a14ece8b6b2
MiloQin764 <MiloQin764@users.noreply.github.com>
2024-03-06 02:56:01 +0000

    deploy: 787bf631971c8addc8b0301dd70754cd45f0dedb

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DungeonCat-DungeonCat-0.0.147";
+const cacheName = "DungeonCat-DungeonCat-0.0.149";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js.unityweb",
