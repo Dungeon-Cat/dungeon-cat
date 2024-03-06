c89b6ed37cb15d05bca76d3dfb9a29ff61a206fe
doombubbles <doombubbles@users.noreply.github.com>
2024-03-06 08:09:57 +0000

    deploy: e16d75ced512735d12d7ff3df8463c4f0db3e1a3

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DungeonCat-DungeonCat-0.0.159";
+const cacheName = "DungeonCat-DungeonCat-0.0.161";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js.unityweb",
