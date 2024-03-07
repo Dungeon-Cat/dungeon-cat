99cd176e7581771afecc58c819314567c3792076
Brandon <35272064+jesssup@users.noreply.github.com>
2024-03-06 18:29:07 -0800

    Added info statue for dialogue

---
+++ DungeonCat/Assets/Scripts/Components/Room3/InfoStatue.cs
@@ -0,0 +1,22 @@
+﻿using Scripts.Components.CommonEntities;
+using Scripts.Components.Room1;
+using Scripts.Components.UI;
+using UnityEngine;
+
+namespace Scripts.Components.Room3
+{
+    public class InfoStatue : MonoBehaviour, IInteractable
+    {
+        private const string Author = "Witch Statue";
+        public bool dialogueTriggered;
+        
+        public bool CanBeInteractedWith() => !dialogueTriggered;
+        public new void Interact()
+        {
+            dialogueTriggered = true;
+            UnityState.Instance.dialogue.StartInteraction(new Interaction(
+                new DialogueLine(Author, "Curiously, these old ruins seem to be built specifically so that somecat can solve them...").AuthorColor(Color.blue).TextColor(Color.black)
+            ));
+        }
+    }
+}
